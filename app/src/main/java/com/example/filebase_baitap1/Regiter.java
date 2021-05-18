633aaa917ff53897ec35120bc074b1fdbd705f3b
Admin <mynguyen8333@gmail.com>
2021-05-18 10:57:03 +0700

    lan4

--- app/src/main/java/com/example/filebase_baitap1/Regiter.java
+++ app/src/main/java/com/example/filebase_baitap1/Regiter.java
@@ -2,13 +2,30 @@ package com.example.filebase_baitap1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Regiter extends AppCompatActivity {
+    TextView tv_opensingin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regiter);
+
+        tv_opensingin = findViewById(R.id.txt_opensignin);
+        tv_opensingin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoDangKy();
+            }
+        });
+    }
+    private void MoDangKy()
+    {
+        Intent intent = new Intent(Regiter.this,Sign_in.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/filebase_baitap1/Sign_in.java
+++ app/src/main/java/com/example/filebase_baitap1/Sign_in.java
@@ -2,13 +2,29 @@ package com.example.filebase_baitap1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Sign_in extends AppCompatActivity {
-
+    TextView tv_openRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        tv_openRegister = findViewById(R.id.txt_openRegister);
+        tv_openRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoDangNhap();
+            }
+        });
+
+    }
+    private void MoDangNhap()
+    {
+        Intent intent = new Intent(Sign_in.this,Regiter.class);
+        startActivity(intent);
     }
 }
