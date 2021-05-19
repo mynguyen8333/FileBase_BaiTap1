e7aa7060a84d4c7f565de0dd13730f09f17f3931
Admin <mynguyen8333@gmail.com>
2021-05-19 11:06:59 +0700

    lan5

--- app/src/main/java/com/example/filebase_baitap1/Sign_in.java
+++ app/src/main/java/com/example/filebase_baitap1/Sign_in.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,6 +28,7 @@ public class Sign_in extends AppCompatActivity {
     TextView tv_openRegister;
     EditText username,password;
     Button btnSignin;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +38,7 @@ public class Sign_in extends AppCompatActivity {
         username = findViewById(R.id.editname_signin);
         password = findViewById(R.id.editpass_singin);
         btnSignin = findViewById(R.id.btnSignin_signin);
+        firebaseAuth = FirebaseAuth.getInstance();
 
 
         tv_openRegister.setOnClickListener(new View.OnClickListener() {
@@ -43,10 +51,33 @@ public class Sign_in extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 isUser();
+//                String tex_email = username.getText().toString();
+//                String tex_pass = password.getText().toString();
+//                if(TextUtils.isEmpty(tex_email)||TextUtils.isEmpty(tex_pass)){
+//                    Toast.makeText(Sign_in.this,"Chua nhap du du lieu",Toast.LENGTH_SHORT).show();
+//                }else{
+//                    login(tex_email,tex_pass);
+//                }
             }
         });
 
     }
+
+    private void login(String tex_email, String tex_pass) {
+        firebaseAuth.signInWithEmailAndPassword(tex_email,tex_pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(Sign_in.this,GiaoDienChinh.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(Sign_in.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
     private void MoDangNhap()
     {
         Intent intent = new Intent(Sign_in.this,Regiter.class);
@@ -70,7 +101,7 @@ public class Sign_in extends AppCompatActivity {
 
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
 
-        Query checkUser = reference.orderByChild("email").equalTo(userEnteredUserName);
+        Query checkUser = reference.orderByChild("name").equalTo(userEnteredUserName);
 
         checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -81,8 +112,10 @@ public class Sign_in extends AppCompatActivity {
                     if(passwordFromDB.equalsIgnoreCase(userEnteredPassWord)){
                         MoGiaoDienChinh();
                     }else{
-                        Toast.makeText(Sign_in.this,"Sai ten tai khoan hoac mat khau",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Sign_in.this,"Sai mat khau",Toast.LENGTH_SHORT).show();
                     }
+                }else{
+                    Toast.makeText(Sign_in.this,"Sai ten tai khoan",Toast.LENGTH_SHORT).show();
                 }
             }
 
